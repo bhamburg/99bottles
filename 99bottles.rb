8963a1e1b24058d605e4f0b7d2ddaff52f104bb5
Brian Hamburg <bhamburg@gmail.com>
2014-03-20 09:10:27 -0400

    changed print to puts

--- 99bottles.rb
+++ 99bottles.rb
@@ -1,17 +1,19 @@
 # Get user's favorite beer
-print "What is your favorite beer? (default is Sam Adams)\n"
+puts "What is your favorite beer? (default is Sam Adams)"
 beer = gets.chomp.split.map(&:capitalize).join(' ')
 beer = "Sam Adams" if beer == ""
 
 # Get user's number of bottles
-print "How many bottles of #{beer} do you have? (default is 99)\n"
+puts "How many bottles of #{beer} do you have? (default is 99)"
 n = gets.to_i
 n = 99 if n == 0
 
 # Print lyrics
 while n > 0
     s = "s" unless n == 1
-    puts "#{n} bottle#{s} of #{beer} on the wall.\n#{n} bottle#{s} of #{beer}.\nTake one down, pass it around."
+    puts "#{n} bottle#{s} of #{beer} on the wall."
+    puts "#{n} bottle#{s} of #{beer}."
+    puts "Take one down, pass it around."
     n -= 1
     s = "" if n == 1
     puts "#{n} bottle#{s} of #{beer} on the wall.\n\n"
